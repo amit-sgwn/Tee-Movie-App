764324f1b54a0869a17ca5a7875ad131372bf93b
Amit Kumar <y13uc022@lnmiit.ac.in>
2016-06-12 18:50:16 +0530

    Added Forecast and ForecastFragment file

--- app/src/main/java/com/example/amit/themovieapp/SettingsActivity.java
+++ app/src/main/java/com/example/amit/themovieapp/SettingsActivity.java
@@ -33,6 +33,18 @@ import java.util.List;
  * Android Design: Settings</a> for design guidelines and the <a
  * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
  * API Guide</a> for more information on developing a Settings UI.
+ *
+ * * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 public class SettingsActivity extends AppCompatPreferenceActivity {
     /**
